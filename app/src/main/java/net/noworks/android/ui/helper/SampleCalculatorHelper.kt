3c3e0d9021ba3f50c679f5ed0db0b8941151914d
izuno4t <tadashi.izuno@gmail.com>
2018-09-30 15:28:04 +0900

    KDoc追加

--- app/src/main/java/net/noworks/android/ui/helper/SampleCalculatorHelper.kt
+++ app/src/main/java/net/noworks/android/ui/helper/SampleCalculatorHelper.kt
@@ -1,7 +1,25 @@
 package net.noworks.android.ui.helper
 
+/**
+ * ユニットテスト用のサンプル実装
+ *
+ */
 class SampleCalculatorHelper {
+    /**
+     * 加算した結果を返します。
+     *
+     * @param x
+     * @param y
+     * @return 加算した結果
+     */
     fun sum(x: Int, y: Int): Int = x + y
- 
+
+    /**
+     * 減算した結果を返します。
+     *
+     * @param x
+     * @param y
+     * @return 減算した結果
+     */
     fun subtract(x: Int, y: Int): Int = x - y
 }
